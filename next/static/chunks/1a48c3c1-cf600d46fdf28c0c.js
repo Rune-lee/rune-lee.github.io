8b4edf9360339eb3e78c65281c8fee19da476d29
codeNcool <codeNcool@outlook.com>
2024-08-27 07:12:26 -0700

    initial commit

---
+++ next/static/chunks/1a48c3c1-cf600d46fdf28c0c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[874],{7516:function(t,n,c){c.d(n,{p4i:function(){return a}});var u=c(8357);function a(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553-6-3.428-6 3.428V4h12v14.553z"}}]})(t)}}}]);
